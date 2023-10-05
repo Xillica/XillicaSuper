338d81f74061bfda8f3a292ead4698dc9b6ef3c8
gamagegmp <malithipgamage@gmail.com>
2023-10-05 14:44:29 +0530

    home page ui

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,47 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container, Grow } from "@mui/material";
 import Grid from "@mui/system/Unstable_Grid";
-import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
 import styled from "@mui/system/styled";
+import ConsoleTextAnimation from "./ConsoleTextAnimation"; // Import the ConsoleTextAnimation component
 import "./styles.css";
 
 const Item = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  border: "1px solid",
+  border: "0px solid",
   borderColor: theme.palette.mode === "dark" ? "#444d58" : "#ced7e0",
-  padding: theme.spacing(2),
+  padding: theme.spacing(1),
   borderRadius: "50px",
+  height: "10px",
   textAlign: "center",
 }));
 
-//Register the ScrollTrigger plugin with GSAP
-gsap.registerPlugin(ScrollTrigger);
+const VideoPlayer = ({ videoSource }) => {
+  return (
+    <div className="video-container">
+      <video src={videoSource} autoPlay loop className="video-element" />
+    </div>
+  )};
 
 const Home = () => {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  //console.log(user);
-
-  useEffect(() => {
-    setUser(JSON.parse(localStorage.getItem("profile")));
-  }, []);
-
   return (
     <Container maxWidth="100%" style={{ marginTop: "150px" }}>
-      <Grow in>
-        <Grid container spacing={2}>
-          <Grid xs>
-          </Grid>
-          <Grid xs={4}>
-            <Item></Item>
-          </Grid>
-          <Grid xs={4}>
-            <Item></Item>
-          </Grid>
-          <Grid xs>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Item>
+            <ConsoleTextAnimation /> {/* Include the animation here */}
+          </Item>
         </Grid>
-      </Grow>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <VideoPlayer videoSource="video1.mp4" />    
+        </Grid>
+      </Grid>
     </Container>
   );
 };
